bcf3acf9c9c378406d756a934d2fb4b3a4cb09ef
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-18 14:13:08 -0400

    add basic server impl

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
-def findById(id):
-    pass
+documents = dict()
 
-def addDocument(x):
-    pass
+def findById(id):
+    if id not in documents:
+        return 'Not Found', 404
+    return documents[id]
+
+def addDocument(id, body):
+    if id in documents:
+        return 'Document already exists', 409
+    documents[id] = body
 
 def similarById(id):
     pass

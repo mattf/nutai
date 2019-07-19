48114f44ef4e1f876acd1de78431fa88f5255c21
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-19 15:14:37 -0400

    add basic bulk upload

--- api.py
+++ api.py
@@ -8,6 +8,15 @@ ids = np.ndarray((0,), dtype='<U42') # TODO: find appropriate id length
 sigs = np.ndarray((0, 42), dtype=int)
 hash_funcs = list(minhash.generate_hash_funcs(42))
 
+def addDocuments(body):
+    accepted = []
+    rejected = []
+    for doc in body:
+        if addDocument(doc['id'], doc['content']):
+            rejected.append(doc['id'])
+        else:
+            accepted.append(doc['id'])
+    return {"accepted": accepted, "rejected": rejected}
 
 def addDocument(id, body):
     if id in ids:

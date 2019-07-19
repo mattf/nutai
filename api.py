0bb4701d79c38247ededd59abc13c82bffd98455
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-18 21:58:00 -0400

    simplify api to POST new doc & GET similar docs

--- api.py
+++ api.py
@@ -1,10 +1,5 @@
 documents = dict()
 
-def findById(id):
-    if id not in documents:
-        return 'Not Found', 404
-    return documents[id]
-
 def addDocument(id, body):
     if id in documents:
         return 'Document already exists', 409

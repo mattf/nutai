38af786ba8e737f33bea7a9258cdf1573fe34fb2
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-19 14:36:35 -0400

    add basic minhash impl

--- api.py
+++ api.py
@@ -1,11 +1,35 @@
-documents = dict()
+import numpy as np
+import minhash
+
+# TODO: remove magic 42
+
+print("initializing...")
+ids = np.ndarray((0,), dtype='<U42') # TODO: find appropriate id length
+sigs = np.ndarray((0, 42), dtype=int)
+hash_funcs = list(minhash.generate_hash_funcs(42))
+
 
 def addDocument(id, body):
-    if id in documents:
+    if id in ids:
         return 'Document already exists', 409
-    documents[id] = body
+
+    # TODO: make growing efficient
+    ids.resize((len(ids) + 1,))
+    sigs.resize((len(sigs) + 1, 42))
+
+    ids[-1] = id
+    shingles = list(minhash.generate_shingles(body.split(" ")))
+    sigs[-1] = minhash.calculate_signature(shingles, hash_funcs)
+
 
 def similarById(id):
-    if id not in documents:
+    if id not in ids:
         return 'Not Found', 404
-    return [{"id": id, "score": 0} for id in documents.keys()]
+
+    sig = sigs[ids == id][0]
+    scores = minhash.approx_jaccard_score(sig, sigs, 1)
+    hits = scores > .42 # TODO: find appropriate threshold
+
+    return [{"id": id, "score": score}
+            for id, score in zip(ids[hits],
+                                 (scores[hits]*100).astype(int).tolist())]

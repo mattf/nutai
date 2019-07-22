51772d33c5c1559b8c059f5cb079e0d5840b12b1
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-22 16:04:00 -0400

    refactor adding MockRedis context guard

--- test_api.py
+++ test_api.py
@@ -2,34 +2,34 @@ import redis
 
 import api
 
-def test_get_unknown_document():
-    # mock up Redis
-    fake_redis = api.NullRedis()
-    redis.Redis = lambda: fake_redis
+class MockRedis:
+    def __enter__(self):
+        # mock up Redis
+        fake_redis = api.NullRedis()
+        self.real_redis = redis.Redis
+        redis.Redis = lambda: fake_redis
+    def __exit__(self, type, value, traceback):
+        redis.Redis = self.real_redis
 
-    nut = api.Nut()
-    _, code = nut.similarById("0")
-    assert code == 404
+def test_get_unknown_document():
+    with MockRedis():
+        nut = api.Nut()
+        _, code = nut.similarById("0")
+        assert code == 404
 
 def test_add_single_document():
-    # mock up Redis
-    fake_redis = api.NullRedis()
-    redis.Redis = lambda: fake_redis
-
-    nut = api.Nut()
-    assert nut.addDocument("0", "a b c") is None
-    assert len(nut.similarById("0")) == 1
-    assert nut.status()["_end"] == 1
+    with MockRedis():
+        nut = api.Nut()
+        assert nut.addDocument("0", "a b c") is None
+        assert len(nut.similarById("0")) == 1
+        assert nut.status()["_end"] == 1
 
 def test_add_duplicate_document():
-    # mock up Redis
-    fake_redis = api.NullRedis()
-    redis.Redis = lambda: fake_redis
-
-    nut = api.Nut()
-    assert nut.addDocument("0", "a b c") is None
-    assert len(nut.similarById("0")) == 1
-    assert nut.status()["_end"] == 1
-    _, code = nut.addDocument("0", "x y z")
-    assert code == 409
-    assert nut.status()["_end"] == 1
+    with MockRedis():
+        nut = api.Nut()
+        assert nut.addDocument("0", "a b c") is None
+        assert len(nut.similarById("0")) == 1
+        assert nut.status()["_end"] == 1
+        _, code = nut.addDocument("0", "x y z")
+        assert code == 409
+        assert nut.status()["_end"] == 1

0c6f93b39ea5efec0bbf75a174ca28ecf8bfce49
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-22 15:58:25 -0400

    add test for adding a single document

--- test_api.py
+++ test_api.py
@@ -10,3 +10,13 @@ def test_get_unknown_document():
     nut = api.Nut()
     _, code = nut.similarById("0")
     assert code == 404
+
+def test_add_single_document():
+    # mock up Redis
+    fake_redis = api.NullRedis()
+    redis.Redis = lambda: fake_redis
+
+    nut = api.Nut()
+    assert nut.addDocument("0", "a b c") is None
+    assert len(nut.similarById("0")) == 1
+    assert nut.status()["_end"] == 1

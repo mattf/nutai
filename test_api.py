028518cb79b46033cdbb74491ccf716e281393ba
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-22 16:00:29 -0400

    add test for adding a duplicate document

--- test_api.py
+++ test_api.py
@@ -20,3 +20,16 @@ def test_add_single_document():
     assert nut.addDocument("0", "a b c") is None
     assert len(nut.similarById("0")) == 1
     assert nut.status()["_end"] == 1
+
+def test_add_duplicate_document():
+    # mock up Redis
+    fake_redis = api.NullRedis()
+    redis.Redis = lambda: fake_redis
+
+    nut = api.Nut()
+    assert nut.addDocument("0", "a b c") is None
+    assert len(nut.similarById("0")) == 1
+    assert nut.status()["_end"] == 1
+    _, code = nut.addDocument("0", "x y z")
+    assert code == 409
+    assert nut.status()["_end"] == 1

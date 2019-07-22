32bbf9fc58f46ce4bd94cd0e26b3ea38579220e0
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-21 22:09:52 -0400

    pull up a _catch_up method to load unknown (id,sig) pairs

--- api.py
+++ api.py
@@ -23,24 +23,45 @@ class Store:
         len_ = max(self.r.llen(self.key), 42) # if redis is empty, don't start w/ 0 len
         self.ids = np.ndarray((len_,), dtype='<U42') # TODO: find appropriate id length
         self.sigs = np.ndarray((len_, 42), dtype=int)
-        for raw in self.r.lrange(self.key, 0, len_):
-            data = json.loads(raw)
-            self.ids[self._end] = data['id']
-            self.sigs[self._end] = np.array(data['sig'])
-            self._end += 1
-        print("loaded", self._end, "signatures")
+        print("loaded", self._catch_up(), "signatures")
 
     def __contains__(self, id):
         return id in self.ids
 
+    def _catch_up(self):
+        count = self._end
+        # _end represents the number of (id,sig) pairs known locally
+        # llen(key) represents the number of (id,sig) pairs known globally
+        # local knowledge should never be ahead of global knowledge
+        # while local knowledge trails global,
+        #    pull down additional global knowledge
+        len_ = self.r.llen(self.key)
+        while self._end + 1 < len_:
+            unknown = self.r.lrange(self.key, self._end + 1, len_)
+            self.ids.resize((len_,))
+            self.sigs.resize((len_, 42))
+            for raw in unknown:
+                pair = json.loads(raw)
+                self.ids[self._end] = pair['id']
+                self.sigs[self._end] = np.array(pair['sig'])
+                self._end += 1
+            len_ = self.r.llen(self.key) # TODO: find a way to avoid infinite loop
+        return self._end - count + 1
+
     def add(self, id, sig):
+        print("loaded", self._catch_up(), "signatures")
         if self._end == len(self.ids):
             self.ids.resize((int(self._end * 1.25),))
             self.sigs.resize((int(self._end * 1.25), 42))
         self.ids[self._end] = id
         self.sigs[self._end] = sig
         self._end += 1
-        self.r.rpush(self.key, json.dumps({"id":id,"sig":sig.tolist()}))
+        i = self.r.rpush(self.key, json.dumps({"id":id,"sig":sig.tolist()}))
+        # TODO: _end should never be > i
+        # TODO: handle _end < i, aka there was an addition between start of
+        #       this method and the rpush need to catch up, and it's ok if
+        #       local and remote lists are not in the same order, as long as
+        #       they have the same contents
 
 
 print("initializing...")

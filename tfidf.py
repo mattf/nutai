9c214f37d30d5366d6d43cec1e725be01e7eb926
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-29 07:12:13 -0400

    introduce tqdm, replace uses of Timer

--- tfidf.py
+++ tfidf.py
@@ -3,6 +3,7 @@ import time
 
 import gensim
 import numpy as np
+from tqdm import tqdm
 
 class Timer:
     def __init__(self, message=None):
@@ -17,41 +18,39 @@ class Timer:
             print(self.message, ":", self.interval)
 
 def __main__():
-    with Timer("load time"):
-        with open("docs.json") as fp:
-            data = json.load(fp)
-            ids = np.array([doc['id'] for doc in data])
-            texts = [gensim.utils.simple_preprocess(doc['text']) for doc in data]
-        print(len(ids), ":", " ".join(map(str,ids[1:5])), "...", " ".join(map(str,ids[-4:])))
+    with open("docs.json") as fp:
+        data = json.load(fp)
+        ids = np.zeros((len(data),), dtype=int)
+        texts = []
+        for i, doc in enumerate(tqdm(data, "loading")):
+            ids[i] = doc['id']
+            texts.append(gensim.utils.simple_preprocess(doc['text']))
+    print(len(ids), ":", " ".join(map(str,ids[1:5])), "...", " ".join(map(str,ids[-4:])))
 
-    with Timer("dictionary build time"):
-        dictionary = gensim.corpora.Dictionary(texts)
-        dictionary.filter_extremes()
-        dictionary.compactify()
+    dictionary = gensim.corpora.Dictionary(tqdm(texts, desc="building dictionary"))
+    dictionary.filter_extremes()
+    dictionary.compactify()
 
     with Timer("model build time"):
         tfidf = gensim.models.TfidfModel(dictionary=dictionary)
 
-    with Timer("index build time"):
-        vecs = [dictionary.doc2bow(text) for text in texts]
-        index = gensim.similarities.MatrixSimilarity(tfidf[vecs])
+    vecs = [dictionary.doc2bow(text) for text in tqdm(texts, desc="building index")]
+    index = gensim.similarities.MatrixSimilarity(tfidf[vecs])
 
-    with Timer("score time"):
-        scores = [sim for sim in index]
+    scores = [sim for sim in tqdm(index, desc="scoring")]
 
-    with Timer("bin time"):
-        # np.histogram uses last bin as max, to include 1.0 need a bin >1.0
-        bins = (0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 42)
-        hist = {0: 0, .1: 0, .2: 0, .3: 0, .4: 0, .5: 0, .6: 0, .7: 0, .8: 0, .9: 0, 1: 0}
-        for row in scores:
-            counts, _ = np.histogram((row*10).astype(int)/10, bins)
-            for i, c in enumerate(counts):
-                hist[bins[i]] += c
+    # np.histogram uses last bin as max, to include 1.0 need a bin >1.0
+    bins = (0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 42)
+    hist = {0: 0, .1: 0, .2: 0, .3: 0, .4: 0, .5: 0, .6: 0, .7: 0, .8: 0, .9: 0, 1: 0}
+    for row in tqdm(scores, desc="binning"):
+        counts, _ = np.histogram((row*10).astype(int)/10, bins)
+        for i, c in enumerate(counts):
+            hist[bins[i]] += c
     print(hist)
 
     with open("discovered_dups", "w") as fp:
         threshold = .7
-        for i, row in enumerate(scores):
+        for i, row in enumerate(tqdm(scores, desc="discovery")):
             hits = np.logical_and(row > threshold, row < 1)
             for id_, score in zip(ids[hits], row[hits]):
                 print(ids[i], id_, score, file=fp)

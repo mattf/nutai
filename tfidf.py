5488acfdc86c2f5ad0559cbc291568b953fe229d
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-31 12:23:54 -0400

    remove histogram and discovery from tfidf

--- tfidf.py
+++ tfidf.py
@@ -55,23 +55,5 @@ def __main__():
         with open("scores", 'wb') as fp:
             msgpack.dump(scores, fp)
 
-    # np.histogram uses last bin as max, to include 1.0 need a bin >1.0
-    bins = (0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 42)
-    hist = {0: 0, .1: 0, .2: 0, .3: 0, .4: 0, .5: 0, .6: 0, .7: 0, .8: 0, .9: 0, 1: 0}
-    for row in tqdm(scores, desc="binning"):
-        counts, _ = np.histogram(row.round(3), bins)
-        for i, c in enumerate(counts):
-            hist[bins[i]] += c
-    print(hist)
-
-    with open("discovered_dups", "w") as fp:
-        threshold = .7
-        for i, row in enumerate(tqdm(scores, desc="discovery")):
-            hits = np.logical_and(np.logical_and(row > threshold, row < 1),
-                np.array([False]*(i+1) + [True]*(len(row)-i-1)))
-            for id_, score in zip(ids[hits], row[hits]):
-                print(ids[i], id_, score, file=fp)
-
-
 if __name__ == "__main__":
     __main__()

12515cbfc766b53a23b2c3beb700443d8583df58
Matthew Farrellee <matt@cs.wisc.edu>
2019-08-05 14:37:22 -0400

    add doc2vec model

---
+++ doc2vec.py
@@ -0,0 +1,27 @@
+import gensim
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.utils import simple_preprocess
+import numpy as np
+from tqdm import tqdm
+
+from helpers import load_texts, save_scores
+from timer import Timer
+
+def __main__():
+    ids, texts = load_texts("solutions.json")
+    tagged_docs = [TaggedDocument(text, [id_]) for id_, text in zip(ids, texts)]
+
+    model = Doc2Vec()
+    model.build_vocab(tqdm(tagged_docs, desc="building vocab"))
+
+    model.train(tqdm(tagged_docs, desc="training"),
+        total_examples=model.corpus_count, epochs=model.epochs)
+
+    def score_generator(ids, model):
+        for id_ in ids:
+            yield model.docvecs.distances(id_)
+
+    save_scores(ids, score_generator(ids, model))
+
+if __name__ == "__main__":
+    __main__()

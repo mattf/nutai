979ed0e39d931c56a279fa84858097e8300852b1
Matthew Farrellee <matt@cs.wisc.edu>
2019-08-05 13:30:10 -0400

    use new helper functions

--- minhash.py
+++ minhash.py
@@ -5,14 +5,12 @@ import zlib
 
 import gensim
 from gensim.utils import simple_preprocess, iter_windows
-import msgpack
-import msgpack_numpy
 import numpy as np
 from tqdm import tqdm
 
+from helpers import load_texts, save_scores
 from timer import Timer
 
-msgpack_numpy.patch()
 
 def generate_shingles(words, count=2, mapper=lambda x: zlib.adler32(x.encode())):
     if len(words) < count:
@@ -52,53 +50,21 @@ def __main__():
     print("using seed:", seed)
     print("signature length:", sig_len)
 
-    with open("solutions.json") as fp:
-        data = json.load(fp)
-        ids = []
-        texts = []
-        missing_issue = 0
-        missing_body = 0
-        missing_both = 0
-        for i, doc in enumerate(tqdm(data, desc="loading docs")):
-            text = str()
-            if 'issue' not in doc:
-                missing_issue += 1
-            else:
-                text += ' '.join(doc['issue'])
-            if 'body' not in doc:
-                missing_body += 1
-            else:
-                text += ' '.join(doc['body'])
-            if len(text) == 0:
-                missing_both += 1
-            else:
-                ids.append(doc['solution.id'])
-                texts.append(gensim.utils.simple_preprocess(text))
-        ids = np.array(ids)
-        num_docs = len(ids)
-    print("missing issues", missing_issue)
-    print("missing body", missing_body)
-    print("skipped solutions (missing both)", missing_both)
-    print(len(ids), ":", " ".join(map(str,ids[1:5])), "...", " ".join(map(str,ids[-4:])))
+    ids, texts = load_texts("solutions.json")
 
     hash_funcs = list(generate_hash_funcs(sig_len))
 
-    sigs = np.empty((num_docs, sig_len))
+    sigs = np.empty((len(ids), sig_len))
     for i, text in enumerate(tqdm(texts, desc="signatures")):
         shingles = list(generate_shingles(text))
         sigs[i] = calculate_signature(shingles, hash_funcs)
     print(sigs)
 
-    scores = np.ndarray((num_docs, num_docs), dtype='uint8')
-    for i, sig in enumerate(tqdm(sigs, desc="scoring")):
-        scores[i] = approx_jaccard_score(sig, sigs, 1)*100
-    print(scores)
+    def score_generator(sigs):
+        for sig in sigs:
+            yield approx_jaccard_score(sig, sigs, 1)
 
-    with Timer("saving time"):
-        with open("ids", 'wb') as fp:
-            msgpack.dump(ids, fp)
-        with open("scores", 'wb') as fp:
-            msgpack.dump(scores, fp)
+    save_scores(ids, score_generator(sigs))
 
 if __name__ == "__main__":
     __main__()
